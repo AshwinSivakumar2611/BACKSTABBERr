dfeccb6e5c628fcecddea6f73ef1b391c2be5ac2
AshwinSivakumar2611 <53612598+AshwinSivakumar2611@users.noreply.github.com>
2019-11-09 18:20:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var database;
+var gameState=0;
+var playerCount;
+var form,player,game;
+var welcomeScreen,homeScreen,sampleMap,imageVar;
+var allPlayers;
+var player1,player2,player3,player4,players;
+
+function preLoad(){
+
+}
+
+function setup(){
+    database = firebase.database();
+    createCanvas(displayWidth-30,displayHeight-148);
+    welcomeScreen = loadImage("./images/WelcomeScreen.png");
+    homeScreen = loadImage("./images/HomeScreen.png");
+    sampleMap = loadImage("./images/SampleMap.png");
+
+    imageVar=welcomeScreen;
+
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+
+function draw(){      
+    background(imageVar);
+    if (playerCount===4){
+        game.updateState(1);
+    }
+    if (gameState===1){
+        game.play();
+        //imageVar=sampleMap;
+    }
+    drawSprites();
+}
+
+function keyPressed(){
+    if (keyCode === 32 && gameState===0){
+        form = new Form();
+        form.display();
+        imageVar=homeScreen;
+    }
+}
